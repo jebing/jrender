2e51f0d50427e6342975789ef169c2f5c09de9ed
hendra.sasmita <hendra.sasmita>
2025-07-27 21:32:50 +0800

    REV-176: fix table schema for api-keys

--- resources/migrations/000005_api_keys_table.up.sql
+++ resources/migrations/000005_api_keys_table.up.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS api_keys (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(id),
+    user_id UUID NOT NULL REFERENCES customers(id),
     expires_at TIMESTAMP WITH TIME ZONE,
     revoked_at TIMESTAMP WITH TIME ZONE,
     status SMALLINT NOT NULL DEFAULT 1, -- 1: active, 2: revoked, 3: expired
--- webapp/app.go
+++ webapp/app.go
@@ -26,7 +26,7 @@ func Run(r *chi.Mux) {
 
 func New(r *chi.Mux) *App {
 	s := &http.Server{
-		Addr:         ":9000",
+		Addr:         ":9200",
 		Handler:      r,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 60 * time.Second,
